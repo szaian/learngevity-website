e933e75789cda7f380da29a5c5cf8e4efdd8e38d
szaian <122959313+szaian@users.noreply.github.com>
2025-12-10 12:29:23 +0200

    making tutor-login visible

--- public/js/supabaseClient.js
+++ public/js/supabaseClient.js
@@ -1,10 +1,10 @@
-// Load Supabase (dynamic import because CDN can't be used inside .js files)
-import("https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2").then(({ createClient }) => {
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-  window.supabase = createClient(
-    "https://edhwvkvmedignpngpxkw.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVkaHd2a3ZtZWRpZ25wbmdweGt3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ5Nzk1NzksImV4cCI6MjA4MDU1NTU3OX0.3IagtxjgsUpDA-kjzp1Dgf9bb8-tVsgTzbHgg6STRbQ"
-  );
+console.log("Supabase Loaded Successfully");
 
-  console.log("Supabase Loaded Successfully");
-});
+// Make Supabase client global
+window.supabase = createClient(
+  "https://edhwvkvmedignpngpxkw.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVkaHd2a3ZtZWRpZ25wbmdweGt3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ5Nzk1NzksImV4cCI6MjA4MDU1NTU3OX0.3IagtxjgsUpDA-kjzp1Dgf9bb8-tVsgTzbHgg6STRbQ"
+);
+
